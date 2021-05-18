de6692a6b22a36615cfc1ecba2fb8063a79347b4
wardie93 <wardie93@msn.com>
2021-05-18 21:41:03 +0100

    Sort directive applies a style that adds padding ready for the icon to go in
    Signed-off-by: wardie93 <wardie93@msn.com>

--- projects/ngx-header-sort/src/lib/ngx-header-sort.directive.ts
+++ projects/ngx-header-sort/src/lib/ngx-header-sort.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[ngxHeaderSort]'
 })
 export class NgxHeaderSortDirective {
-    constructor() {}
+    constructor(
+        private readonly element: ElementRef,
+        private readonly renderer: Renderer2
+    ) {
+        this.renderer.addClass(this.element.nativeElement, 'ngx-header-sort');
+    }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-header-sort-app';
+    people = [
+        {
+            firstName: 'Trycia',
+            lastName: 'Dickens'
+        },
+        {
+            firstName: 'Bennett',
+            lastName: 'Abshire'
+        },
+        {
+            firstName: 'Elijah',
+            lastName: 'Lemke'
+        },
+        {
+            firstName: 'Kendall',
+            lastName: 'Wuckert'
+        },
+        {
+            firstName: 'Candida',
+            lastName: 'Kerluke'
+        },
+        {
+            firstName: 'Fae',
+            lastName: 'Miller'
+        },
+        {
+            firstName: 'Damian',
+            lastName: 'Heaney'
+        },
+        {
+            firstName: 'Rosalia',
+            lastName: 'Carter'
+        },
+        {
+            firstName: 'Julius',
+            lastName: 'Rutherford'
+        },
+        {
+            firstName: 'Axel',
+            lastName: 'Kuhlman'
+        }
+    ];
 }
