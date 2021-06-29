ea60f9c2579f5add59944b34d0a9346fb680d429
wardie93 <wardie93@msn.com>
2021-06-29 19:14:48 +0100

    Can click and change direction of icon displayed to indicate sorting
    Signed-off-by: wardie93 <wardie93@msn.com>

--- projects/ngx-header-sort/src/lib/ngx-header-sort.directive.ts
+++ projects/ngx-header-sort/src/lib/ngx-header-sort.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Inject, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Inject, Input, Renderer2 } from '@angular/core';
 import {
     NgxHeaderSortOptions,
     NGX_HEADER_SORT_OPTIONS
@@ -8,27 +8,66 @@ import {
     selector: '[ngxHeaderSort]'
 })
 export class NgxHeaderSortDirective {
+    @Input('ngxHeaderSort')
+    columnName!: string;
+
+    @HostListener('click')
+    onClickEvent(): void {
+        this.onClick();
+    }
+
+    private iconElement?: ElementRef;
+    private iconAdded = false;
+    private descending = false;
+
     constructor(
         private readonly element: ElementRef,
         private readonly renderer: Renderer2,
         @Inject(NGX_HEADER_SORT_OPTIONS)
         private readonly options: NgxHeaderSortOptions
     ) {
-        this.addSortIcon();
+    }
+
+    private onClick(): void {
+        if (!this.iconAdded) {
+            this.addSortIcon();
+        }
+        else{
+            this.changeDirection();
+        }
     }
 
     private addSortIcon(): void {
-        const element = new ElementRef(this.renderer.createElement('i'));
+        this.iconElement = new ElementRef(this.renderer.createElement('i'));
         this.renderer.appendChild(
             this.element.nativeElement,
-            element.nativeElement
+            this.iconElement.nativeElement
         );
-        this.addClass(element, this.options.ascendingIconClass);
+        this.addClass(this.iconElement, this.options.ascendingIconClass);
+        this.iconAdded = true;
+    }
+
+    private changeDirection(): void {
+        if (this.descending) {
+            this.removeClass(this.iconElement!, 'ngx-header-sort--descending');
+            this.addClass(this.iconElement!, 'ngx-header-sort--ascending');
+        }
+        else{
+            this.removeClass(this.iconElement!, 'ngx-header-sort--ascending');
+            this.addClass(this.iconElement!, 'ngx-header-sort--descending');
+        }
+        this.descending = !this.descending;
     }
 
     private addClass(element: ElementRef, $class: string): void {
         $class.split(' ').forEach(c => {
             this.renderer.addClass(element.nativeElement, c);
+        });
+    }
+
+    private removeClass(element: ElementRef, $class: string): void {
+        $class.split(' ').forEach(c => {
+            this.renderer.removeClass(element.nativeElement, c);
         });
     }
 }
